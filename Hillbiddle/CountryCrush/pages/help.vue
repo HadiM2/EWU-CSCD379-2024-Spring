ae9028d662fec17b32472e9f89f60e21b80a3b8c
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-06-10 15:37:01 -0700

    help changes

--- Hillbiddle/CountryCrush/pages/help.vue
+++ Hillbiddle/CountryCrush/pages/help.vue
@@ -1,23 +1,18 @@
 <template>
-  <v-dialog v-model="modelValue" max-width="500" class="pd text-center custom-card" background="white">
+  <v-dialog v-model="modelValue" max-width="500" >
     <v-card color="secondary">
       <v-sheet color="primary">
         <v-card-text style="text-align: center; font-size: larger">
-          <header>How it works:</header>
+          <header style="font-size: xx-large; font-weight: bold; font-style: italic;" class="pd">How it works:</header>
         </v-card-text>
       </v-sheet>
-      <v-card-text style="text-align: center; font-size: medium">
-        <header>You get one choice</header>
-      </v-card-text>
-      <v-card-text>
-        <ul>
-          <li>If you like what you see: ✅<br> We'll give you their details and you can take it to the next step</li>
-          <br>
-          <li>Not so much?: ❌<br> We'll keep moving forward until you find your match!<br></li>
+      <v-card-text style="text-align: center; font-size: large" class="pd">
+          *If you like what you see: ✅<br> We'll give you their details and you can take it to the next step!
+          <br><br>
+          *Not so much?: ❌<br> We'll keep moving forward until you find your match!<br>
           <br>
           It's that simple! Just keep going until you find your one!
           <br>
-        </ul>
       </v-card-text>
       <v-card-actions>
         <v-btn
--- Hillbiddle/CountryCrush/pages/index.vue
+++ Hillbiddle/CountryCrush/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-divider :width="'100%'"> </v-divider>
-  
+  Welcome!
   <br><br>
 
 
