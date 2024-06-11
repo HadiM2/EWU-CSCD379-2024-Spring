7a048410c6cb8f3f0fc65907c85fd6bf9466882d
Hadi <amallah@ewu.edu>
2024-06-11 04:18:59 -0700

    Landing page working, Made empty matching page
    Banner returns to index, fixed some scripting in index to allow for other animations.

--- Hillbiddle/CountryCrush/pages/index.vue
+++ Hillbiddle/CountryCrush/pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <div ref="character" class="walking-character"></div>
-  <v-divider :width="'100%'"> </v-divider>
+  
   <div>
-    <h1 class="pd">
-      Welcome!
+    <h1 class="pd d-flex justify-center align-center">
+      Welcome Partner!
     </h1>
   </div>
   <br><br>
@@ -12,14 +11,35 @@
       <v-img
         :height="computedImageHeight"
         :width="computedImageWidth"
-        src="../public/landingpage-1.png"
+        src="/CrushPictures/LandingPages/landingPage5.png"
       >
       </v-img>
     </div>
   </v-container>
+  <div>
+    <h1 class="pd d-flex justify-center align-center">
+      YEEHAW!
+    </h1>
+  </div>
+  <v-container>
+    <v-card-actions class="d-flex flex-column flex-md-row justify-center align-center">
+      <v-btn style="background-color: black;" color="secondary" to="/matching"
+        >Start Matching</v-btn
+      >
+      <v-divider class="my-2 my-md-0 mx-md-2"> </v-divider>
+      
+      <v-btn style="background-color: black;" color="secondary" to="/matching"
+        >Start Matching</v-btn
+      >
+      
+    </v-card-actions>
+  </v-container>
+  <div ref="character" class="walking-character"></div>
+  <v-divider :width="'100%'"> </v-divider>
 </template>
 
-<script>
+<script >
+
 export default {
   mounted() {
     this.$nextTick(() => {
@@ -33,6 +53,27 @@ export default {
     });
   }
 };
+</script>
+<script setup>
+import { ref, computed, onMounted, nextTick } from 'vue';
+import { useDisplay } from 'vuetify';
+
+const display = useDisplay();
+
+
+const cardWidth = ref('100%');
+
+const computedImageHeight = computed(() => {
+  if (display.mdAndUp) return 625;
+  if (display.smAndDown) return 300;
+  return 400;
+});
+
+const computedImageWidth = computed(() => {
+  if (display.mdAndUp) return 1800;
+  if (display.smAndDown) return '100%';
+  return '100%';
+});
 </script>
 
 <style scoped>
@@ -82,5 +123,117 @@ export default {
 
 .walk-animation {
   animation-name: walk;
+}
+</style>
+
+<style>
+.index {
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  animation: classicPulse 2s infinite;
+}
+.image-container {
+  width: 100%;
+  max-width: 1800px;
+  animation: peruPulse 2s infinite;
+}
+.simpleButton {
+  width: 100%;
+  animation: classicButton 2s infinite;
+}
+
+.banner {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  background-color: #8747ff21;
+  color: rgb(19, 81, 161);/*blanchedalmond*/
+  font-size: 24px;
+  transform-origin: center center;
+  animation: ban 2s infinite;
+  
+}
+.bannerSmall {
+  padding-right: 20px;
+  padding-left: 0px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-inline-start: 8%;
+  background-color: #8747ff21;
+  color: rgb(19, 81, 161);/*blanchedalmond*/
+  font-size: 24px;
+  transform-origin: center center;
+  animation: ban 2s infinite;
+}
+
+@keyframes peruPulse {
+  0%,
+  100% {
+    filter: drop-shadow(0 0 5px peru) drop-shadow(0 0 10px purple)
+      drop-shadow(0 0 15px peru);
+  }
+  50% {
+    filter: drop-shadow(0 0 10px peru) drop-shadow(0 0 20px purple)
+      drop-shadow(0 0 30px peru);
+  }
+  25% {
+    filter: drop-shadow(-30px 0px 30px rgb(129, 197, 4))
+      drop-shadow(0 10px 10px purple)
+      drop-shadow(30px 0px 30px rgb(1, 156, 228));
+  }
+}
+@keyframes classicPulse {
+  0%,
+  100% {
+    filter: drop-shadow(0 0 5px) drop-shadow(0 0 10px) drop-shadow(0 0 15px);
+  }
+  50% {
+    filter: drop-shadow(0 0 5px) drop-shadow(0 0 10px) drop-shadow(0 0 15px);
+  }
+}
+
+@keyframes classicButton {
+  0%,
+  100% {
+    filter: drop-shadow(0 0 5px blue) drop-shadow(0 0 10px blue)
+      drop-shadow(0 0 15px blue);
+    color: blur();
+  }
+  50% {
+    filter: drop-shadow(0 0 5px blue) drop-shadow(0 0 10px blue)
+      drop-shadow(0 0 15px blue);
+    color: blur();
+  }
+}
+@keyframes ban {
+  0%,
+  100% {
+    transform: skew(0deg, 0deg) scale(1.35, 1.35);
+    filter: saturate(100%) invert(0%);
+  }
+  25% {
+    transform: skew(1deg, 1deg) scale(1.4, 1.4);
+    filter: saturate(180%) invert(10%) hue-rotate(20deg);
+  }
+  50% {
+    transform: skew(2deg, 2deg) scale(1.45, 1.45);
+    filter: saturate(100%) invert(0%) hue-rotate(40deg);
+  }
+  75% {
+    transform: skew(1deg, 1deg) scale(1.4, 1.4);
+    filter: saturate(150%) invert(20%) hue-rotate(60deg);
+  }
+}
+
+@media (max-width: 600px) {
+  .hillbiddle-title {
+    font-size: 8vw; /* Adjust font size for smaller screens */
+  }
+}
+@media (min-width: 1200px) {
+  .hillbiddle-title {
+    font-size: 2vw; /* Adjust font size for larger screens */
+  }
 }
 </style>
